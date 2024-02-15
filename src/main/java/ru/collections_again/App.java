ab1ad2964c024f0795e79eae3a231a8bee837800
abuklov133 <abuklov133@gmail.com>
2024-02-15 20:03:46 +0300

    Стартовые файлы репозитория collection_again

---
+++ src/main/java/ru/collections_again/App.java
@@ -0,0 +1,13 @@
+package ru.collections_again;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
